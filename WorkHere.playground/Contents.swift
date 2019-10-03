177f5156c9ea8e500feaf45eec3a411698f95330
Rayan Ahmed <37629876+rayanazzam@users.noreply.github.com>
2019-10-03 01:56:51 -0500

    Update Contents.swift
    Solution for homework number 3.

--- WorkHere.playground/Contents.swift
+++ WorkHere.playground/Contents.swift
@@ -10,3 +10,37 @@ import Foundation
 // decode those 50 words into an array of strings
 // print the array of words, but with each word reversed
 // print the average number of characters per word
+
+//Here is my answer:
+
+struct Words: Codable {
+  let names: [String]
+}
+
+func printWordsReversed (list: [String]) {
+  for word in list {
+    print(String (word.reversed()))
+  }
+}
+
+func printAverageCharacterPerWord (list: [String]) {
+  var numCharacters = 0
+  for word in list {
+    numCharacters = numCharacters + word.count
+  }
+  print(numCharacters/list.count)50
+}
+let urlString = "https://api.noopschallenge.com/wordbot?count=50"
+
+if let url = URL (string: urlString) {
+  let request = URLRequest(url: url)
+  let session = URLSession(configuration: .ephemeral)
+  
+  let task = session.dataTask(with: request) { (data, response, error) in
+    let wordList = try! JSONDecoder().decode (Words.self, from: data!)
+    printWordsReversed(list: wordList.names)
+    printAverageCharacterPerWord(list: wordList.names)
+  }
+  task.resume()
+}
+
